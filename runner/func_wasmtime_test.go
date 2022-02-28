1ac1b551a63d5cf6161e2104cfb916b4af82462c
wuhuizuo <wuhuizuo@126.com>
2022-02-28 18:10:01 +0800

    feat(runner/wasm/wasmtime): add office go wasm loader
    WIP, test failed.

--- runner/func_wasmtime_test.go
+++ runner/func_wasmtime_test.go
@@ -23,3 +23,20 @@ func Test_wasmtime_tinygo(t *testing.T) {
 		}
 	})
 }
+
+func Test_wasmtime_go(t *testing.T) {
+	t.Skip()
+	t.Run("algorithm", func(t *testing.T) {
+		store, fn := wasmtime.GetGoWasmFuncWithWasmtime(t, filepath.Join(selfDir(t), "..", wasmGo), fibFuncName)
+
+		for _, tt := range fibTests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := fn.Call(store, tt.in)
+
+				assert.NoError(t, err)
+				assert.IsType(t, tt.want, got)
+				assert.Equal(t, tt.want, got)
+			})
+		}
+	})
+}
